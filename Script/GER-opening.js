ce68620df0653c888661781104e35f8236d39610
coiluck <wvy.wingle@gmail.com>
2024-12-11 11:18:41 +0900

    Add 今週前半の進捗: ドイツのキャラ選択画面、ほか

--- Script/GER-opening.js
+++ Script/GER-opening.js
@@ -1,3 +1,10 @@
+// このファイルはドイツ選択時のオープニング処理スクリプトです
+// このファイルでの残タスク一覧
+/* 
+・シナリオを書く
+・話し相手の立ち絵用意
+・キャラ選択処理をOP-Skip時でもうまくいくように書く
+*/
 const openingTextArray = [
   "ここは帝都 ゲルマニア",
   "――世界で最も強大な国の首都",
@@ -13,33 +20,149 @@ const openingTextArray = [
   "",
   "",
 ] // 前史についてなど。うまく書いておいてね、未来の僕。
-let currentOpeningIndex = 0
+let currentOpeningIndex = 0;
 const modalOpening = document.getElementById('GER-modal-opening');
-modalOpening.addEventListener('click', function() {
+const handleClick = function() {
   // 現在のテキストを更新
   const textArea = document.getElementById('GER-opening-text');
   if (textArea) {
     textArea.textContent = openingTextArray[currentOpeningIndex];
     // インデックスを次に進める
     if (currentOpeningIndex + 1 <= openingTextArray.length) {
-        currentOpeningIndex += 1; // 次のインデックスに進む
+      currentOpeningIndex += 1; // 次のインデックスに進む
+    } else {
+      byeOpening(); // 最後ならこのモーダルとのお別れ処理
+    }
+  } else {
+    console.log("テキストコンテナが見つかりません");
+    alert("テキスト更新で意図しないバグが発生しました。製作者に報告してくれると助かります");
+  }
+};
+
+modalOpening.addEventListener('click', handleClick);
+
+function byeOpening() {
+  console.log("Opening終了");
+  modalOpening.removeEventListener('click', handleClick); // イベントリスナーを解除
+  modalOpening.classList.add('fadeout'); // フェードアウト開始
+  setTimeout(function() { 
+    modalOpening.style.display = "none"; 
+  }, 1000);
+  setTimeout(function() {
+    const modalOpeningRoom = document.getElementById("GER-modal-opening-room");
+    modalOpeningRoom.style.display = "flex";
+    modalOpeningRoom.classList.add('fadein');
+  }, 1000); // このモーダルが消えるのと同時にフェードイン開始
+}
+
+
+
+// OP2(Room)
+const opening2TextArray = [
+  "aaa",
+  "bbb",
+  "ccc",
+  "ddd",
+  "eee",
+  "fff",
+  "ggg",
+  "hhh",
+  "iii",
+  "jjj",
+  "kkk",
+  "lll",
+  "mmm",
+  "nnn"
+] 
+let currentOpening2Index = 0
+const modalOpening2 = document.getElementById('GER-modal-opening-room');
+const handleClick2 = function(){
+  const textArea = document.getElementById('GER-opening-room-text');
+  if (textArea) {
+    textArea.textContent = opening2TextArray[currentOpening2Index];
+    // 話し相手の立ち絵表示
+    if (currentOpening2Index === 5) {
+      document.getElementById('opening-room-character').style.display = "block";
+    } 
+    // キャラクター選択を有効化
+    if (currentOpening2Index === 8) {
+      document.body.style.overflow = "hidden"; // スクロールバー無効化しないと画面変になる
+      document.getElementById('GER-modal-character-select').style.display = "block";
+      document.getElementById('GER-modal-character-select').classList.add('fadein');
+      setTimeout(function(){
+        document.body.style.overflow = ""; // 選択モーダル見えたらスクロールバー表示してもいいよ
+      }, 800);
+    } 
+    // インデックスを次に進める
+    if (currentOpening2Index + 1 <= opening2TextArray.length) {
+        currentOpening2Index += 1; // 次のインデックスに進む
       } else {
-        byeOpening(); // 最後ならこのモーダルとのお別れ処理
+        byeOpening2(); // 最後ならこのモーダルとのお別れ処理
       }
   } else {
     console.log("テキストコンテナが見つかりません")
     alert("テキスト更新で意図しないバグが発生しました。製作者に報告してくれると助かります")
   }
-});
+};
 
-function byeOpening() {
-  console.log("Opening終了");
-  document.getElementById('GER-modal-opening').classList.add('fadeout'); // これだとフェードアウト中の1秒間イベントリスナー続く
+modalOpening2.addEventListener('click', handleClick2);
+
+function byeOpening2() {
+  console.log("Opening2終了");
+  modalOpening2.removeEventListener('click', handleClick2);
+  document.getElementById('GER-modal-opening-room').classList.add('fadeout'); // これだとフェードアウト中の1秒間イベントリスナー続く
   setTimeout(function(){ 
-    document.getElementById('GER-modal-opening').style.display = "none"; 
+    document.getElementById('GER-modal-opening-room').style.display = "none"; 
   }, 1000);
   setTimeout(function(){
-    document.getElementById("GER-modal-opening-room").style.display = "flex";
-    document.getElementById("GER-modal-opening-room").classList.add('fadein');
+    document.getElementById("GER-modal-opening-rocket").style.display = "flex";
+    document.getElementById("GER-modal-opening-rocket").classList.add('fadein');
   }, 1000); // このモーダルが消えるのと同時にフェードイン開始
+}
+
+// OP3(Launch)
+const opening3TextArray = [
+  "ccc",
+  "ddd",
+] 
+let currentOpening3Index = 0
+const modalOpening3 = document.getElementById('GER-modal-opening-rocket');
+const handleClick3 = function(){
+  const textArea = document.getElementById('GER-opening-rocket-text');
+  if (textArea) {
+    textArea.textContent = opening3TextArray[currentOpening3Index];
+    // インデックスを次に進める
+    if (currentOpening3Index + 1 <= opening3TextArray.length) {
+        currentOpening3Index += 1; // 次のインデックスに進む
+      } else {
+        byeOpening3(); // 最後ならこのモーダルとのお別れ処理
+      }
+  } else {
+    console.log("テキストコンテナが見つかりません")
+    alert("テキスト更新で意図しないバグが発生しました。製作者に報告してくれると助かります")
+  }
+};
+
+modalOpening3.addEventListener('click', handleClick3);
+
+function byeOpening3() {
+  console.log("Opening3終了");
+  modalOpening3.removeEventListener('click', handleClick3);
+  document.getElementById('GER-modal-opening-rocket').classList.add('fadeout'); // これだとフェードアウト中の1秒間イベントリスナー続く
+  setTimeout(function(){ 
+    document.getElementById('GER-modal-opening-rocket').style.display = "none"; 
+  }, 1000);
+  setTimeout(function(){
+    document.getElementById("GER-modal-game").style.display = "flex";
+    document.getElementById("GER-modal-game").classList.add('fadein');
+  }, 1000); // このモーダルが消えるのと同時にフェードイン開始
+  // ローカルストレージから値を取得
+  const hasSeenGER = localStorage.getItem("hasSeenGER");
+  if (hasSeenGER === "true") {
+    // Good Player :)
+    console.log("知っています: ドイツのオープニング - 複数回目");
+  } else {
+    localStorage.setItem("hasSeenGER", "true");
+    console.log("記録しました: ドイツのオープニング - 読了");
+  }
 }
--- Script/title.js
+++ Script/title.js
@@ -1,102 +1,3 @@
-<<<<<<< HEAD
-document.addEventListener('DOMContentLoaded', () => {
-  // HTMLが読み込まれたら...
-  // タイトル（日独）をフェードインで表示
-  const titleJPN = document.getElementById('title-JPN');
-  const titleGER = document.getElementById('title-GER');
-  titleJPN.style.opacity = "0";
-  titleGER.style.opacity = "0";
-  setTimeout(function(){ 
-    titleJPN.classList.add('fadein-title');
-    titleGER.classList.add('fadein-title');
-  }, 200);
-  console.log("display title-text");
-});
-
-window.addEventListener('load', () => {
-  // 全てのリソースが読み込まれた後に実行
-  const startButton = document.getElementById('start-button');
-  setTimeout(function(){
-    startButton.style.display = "block"; // "見える化、ね"
-    startButton.classList.add('fadein-title-button'); // "おしゃれの本質"
-  }, 1200);
-  // startボタンに下線を引く
-  setTimeout(function(){
-    startButton.classList.add('active');
-  }, 3500);
-  console.log("display start-button");
-});
-
-document.getElementById('start-button').addEventListener('click', event => {
-    // startボタンが押されたら...
-    const titleJPN = document.getElementById('title-JPN');
-    const titleGER = document.getElementById('title-GER');
-    const startButton = document.getElementById('start-button');
-    // 文字全部フェードアウト
-    titleJPN.classList.add('fadeout-title');
-    titleGER.classList.add('fadeout-title');
-    startButton.classList.add('fadeout-title');
-    console.log("clear title-text");
-    // 文字消えてから国家選択表示
-    setTimeout(function(){
-        const countrySection = document.getElementById('country-sections');
-        const selectText = document.getElementById('select-text');
-        const selectGER = document.getElementById('select-GER');
-        const selectJPN = document.getElementById('select-JPN');
-        countrySection.style.display = "flex";
-        countrySection.classList.add('fadein');
-        selectText.style.display = "block";
-        selectText.classList.add('fadein');
-        selectGER.style.display = "block";
-        selectGER.classList.add('fadein');
-        selectJPN.style.display = "block";
-        selectJPN.classList.add('fadein');
-        // 上の部分見づら過ぎておもろい
-        console.log("display country-section");
-    }, 800);
-});
-
-document.getElementById('select-GER').addEventListener('click', function() {
-  // ドイツが選択されたら...
-  const confirmButton = document.getElementById('select-country-confirm')
-  confirmButton.classList.remove('active'); // 一度引いた線を消して..
-  confirmButton.style.display = "block"; // ボタン表示
-  confirmButton.classList.add('fadein');
-  confirmButton.textContent = "総統のために！" // ドイツのボタンに書き換え
-  setTimeout (function(){
-    confirmButton.classList.add('active');
-  }, 500);
-});
-document.getElementById('select-JPN').addEventListener('click', function() {
-  // 日本が選択されたら...
-  const confirmButton = document.getElementById('select-country-confirm')
-  confirmButton.classList.remove('active'); // 一度引いた線を消して..
-  confirmButton.style.display = "block"; // ボタン表示
-  confirmButton.classList.add('fadein');
-  confirmButton.textContent = "天皇万歳！" // 日本のボタンに書き換え
-  setTimeout (function(){
-    confirmButton.classList.add('active');
-  }, 500);
-});
-let selectedCountry; // 関数の外でも取り出せるように宣言
-document.getElementById('select-country-confirm').addEventListener('click', function() {
-  // 国家決定ボタンが押されたら...
-  const forWho = document.getElementById('select-country-confirm').textContent
-  if (forWho === "総統のために！"){
-    console.log("選択: ドイツ")
-    selectedCountry = "GER";
-  } else if (forWho === "天皇万歳！"){
-    console.log("選択: 日本")
-    selectedCountry = "JPN";
-  } else {
-    console.log("選択した国が見つかりません")
-    alert("国家選択で意図しないバグが発生しました。製作者に報告してくれると助かります")
-  }
-  document.getElementById("modal-title").classList.add('fadeout');
-  setTimeout(function(){ 
-    document.getElementById('modal-title').style.display = "none"; 
-  }, 1000);
-=======
 document.addEventListener('DOMContentLoaded', () => {
   // HTMLが読み込まれたら...
   // タイトル（日独）をフェードインで表示
@@ -187,6 +88,17 @@ document.getElementById('select-country-confirm').addEventListener('click', func
       document.getElementById("GER-modal-opening").style.display = "flex";
       document.getElementById("GER-modal-opening").classList.add('fadein');
     }, 1000); // タイトルが消えるのと同時にフェードイン開始
+    setTimeout(function(){
+      const hasSeenGER = localStorage.getItem("hasSeenGER");
+      if (hasSeenGER === "true") {
+        // Skip表示
+        console.log("感知しました: ドイツのオープニング - 複数回目");
+        document.getElementById("skip-window").style.display = "block";
+        document.getElementById("skip-window").classList.add('fadein');
+      } else {
+        console.log("感知しました: ドイツのオープニング - 初回");
+      }
+    }, 1500); // 少し遅れてSkipボタンを表示
   } else if (forWho === "天皇万歳！"){
     console.log("選択: 日本")
     setTimeout(function(){
@@ -201,5 +113,38 @@ document.getElementById('select-country-confirm').addEventListener('click', func
   setTimeout(function(){ 
     document.getElementById('modal-title').style.display = "none"; 
   }, 1000);
->>>>>>> master
 });
+
+// Skipボタンの処理
+const skipCansel = function(){
+  // スキップをキャンセル
+  console.log("選択しました: スキップしない");
+  document.getElementById("skip-cancel").removeEventListener('click', skipCansel); // イベントリスナーを解除
+  document.getElementById("skip-cancel").classList.add('fadeout'); // フェードアウト開始
+  setTimeout(function() { 
+    document.getElementById("skip-cancel").style.display = "none"; 
+  }, 1000);
+}
+const skipButton = function(){
+  // スキップする場合
+  // 見えてるのはフェードアウト、それ以外は見えなくする
+  document.getElementById("GER-modal-opening").classList.add('fadeout'); // フェードアウト開始
+  document.getElementById("GER-modal-opening-room").style.display = "none";
+  document.getElementById("GER-modal-opening-rocket").style.display = "none";
+  setTimeout(function(){ 
+    document.getElementById('GER-modal-opening').style.display = "none"; 
+  }, 1000);
+  // 選択画面を表示
+  document.body.style.overflow = "hidden"; // スクロールバー無効化しないと画面変になる
+  document.getElementById('GER-modal-character-select').style.display = "block";
+  document.getElementById('GER-modal-character-select').classList.add('fadein');
+  setTimeout(function(){
+    document.body.style.overflow = ""; // 選択モーダル見えたらスクロールバー表示してもいいよ
+  }, 800);
+  // 選択画面の確定ボタンを押した後の挙動を変更（これが面倒なのよ）
+  // うおおおおおおおお　上に表示してるだけだから全部消しちゃえば選択画面の下はゲームモーダルだ！！！！
+  // 2日前のおれ神すぎ！！！！
+}
+
+document.getElementById("skip-cancel").addEventListener('click', skipCansel);
+document.getElementById("skip-button").addEventListener('click', skipButton);
